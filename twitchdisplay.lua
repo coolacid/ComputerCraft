86872339c0cef5a73249fa0f3433e018c95b63ba
CoolAcid <jakendall@gmail.com>
2015-02-17 10:17:11 -0500

    cleanup status

--- twitchdisplay.lua
+++ twitchdisplay.lua
@@ -48,40 +48,39 @@ function getFollowers()
 end
 
 function getViewerCount()
-  lstr = http.get("https://api.twitch.tv/kraken/streams/" .. streamid).readAll()
-  lobj = json.decode(lstr)
-  m.setCursorPos(1,1)
-  
-
+  str = http.get("https://api.twitch.tv/kraken/streams/" .. streamid).readAll()
+  lobj = json.decode(str)
   if lobj.stream == nil then
-    m.write(streamid)
-    m.setCursorPos(1,4)
-    m.write("Live Viewers: Offline")
+    return nil
   else
-          live = json.encodePretty(lobj.stream.viewers)
-    m.setBackgroundColor(colors.yellow)
-    m.clear()
-    m.write(streamid)
-    m.setCursorPos(1,4)
-    m.write("Live Viewers: ")
-    m.write(live)
+    return json.encodePretty(lobj.stream.viewers)
   end
-
-  return live
 end
 
 while true do
-  m.setCursorPos(1,1)
-  m.setBackgroundColor(colors.white)
   m.setTextColor(colors.blue)
   m.setTextScale(1)
-  m.clear()
 
-  m.write(streamid)
+  local status, live = pcall(getViewerCount)
+
+  if status then 
+    m.setCursorPos(1,1)
+    if live == nil
+      m.setBackgroundColor(colors.white)
+      m.write(streamid)
+      m.setCursorPos(1,5)  
+      m.write("Live Viewers: Offline")
+    else
+      m.setBackgroundColor(colors.yellow)
+      m.clear()
+      m.write(streamid)
+      m.setCursorPos(1,5)
+      m.write("Live Viewers: " .. live)
+  end
 
   local status, followers, follower = pcall(getFollowers)
 
- if status then
+  if status then
     m.setCursorPos(1,3)  
     m.write("Twitch Followers: " .. followers)
 
@@ -93,15 +92,6 @@ while true do
 
     m.setCursorPos(1,4)
     m.write("Last Follower: ERROR")
-  end
-
-  m.setCursorPos(1,5)  
-  local status, live = pcall(function () getViewerCount() end)
-
-  if status then
-    -- do nothing
-  else
-    m.write("Live Viewers: Loading...")
   end
 
   sleep(SleepTime)
