0ee63e08fe25f894c2ae05ea79b4dcb08a14cc6a
CoolAcid <jakendall@gmail.com>
2015-02-18 16:18:13 -0500

    Proxy *should* work

--- TwitchSub/nodejs/index.js
+++ TwitchSub/nodejs/index.js
@@ -2,8 +2,8 @@
 var port = (process.env.PORT || 5000)
 
 var OAUTHKeys = {
-    waffle: "test",
-    bacon: "test2"
+    user1: "",
+    user2: ""
 }
 var ips = [
     "127.0.0.1"
@@ -16,10 +16,42 @@ var path = require('path'),
     fs = require('fs')
 
 var http = require('http');
+var https = require('https');
+
 http.createServer(function (req, res) {
   httpHandler(req, res);
 }).listen(port);
+
 console.log('Server running at on port: ' + port);
+
+function TwitchPost(stream, token, client_res) {
+    // console.log('stream: ' + stream + ' | token: ' + token);
+    var req_options = {
+      host: 'api.twitch.tv',
+      port: 443,
+      path: '/kraken/channels/' + stream + '/subscriptions',
+      method: 'GET',
+      headers: {
+        'Accept': 'application/vnd.twitchtv.v3+json',
+        'Authorization': 'OAuth ' + token
+      }
+    }
+
+    var preq = https.request(req_options, function(res) {
+      var body = '';
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          body += chunk;
+      });
+      res.on('end', function() {
+        client_res.writeHead(res.statusCode, res.headers);
+        client_res.end(body)
+      });
+    });
+
+    preq.end();
+}
+
 
 function httpHandler(req, res) {
     url = require('url').parse(req.url);
@@ -38,11 +70,13 @@ function httpHandler(req, res) {
         res.end('Not vaild IP');
     // If we have the OAUTH Key defined above, we're good to go. 
     } else if (base in OAUTHKeys) {
-        res.writeHead(200, {'Content-Type': 'application/json'});
         if (testing) {
+            res.writeHead(200, {'Content-Type': 'application/json'});
             var filePath = path.join(__dirname, 'testresponse.json');
             var readStream = fs.createReadStream(filePath);
             readStream.pipe(res);
+        } else {
+            TwitchPost(base, OAUTHKeys[base], res);
         }
     // IP was allowed, but we don't have the Key. 
     } else {
