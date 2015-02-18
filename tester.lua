5f5a0a13dee6a9c41f424ffbc41a3afc20e140f8
CoolAcid <jakendall@gmail.com>
2015-02-18 18:49:35 -0500

    Add a tester script

---
+++ tester.lua
@@ -0,0 +1,20 @@
+-- Simple script to display the type and methods a device has.
+-- Place script advanced computer
+-- Place computer so that device is on the computers back side
+-- Run script.
+-- Profit
+
+-- label set Tester
+-- pastebin get WdiT6sR5 bootstrap
+-- bootstrap
+-- github get coolacid/ComputerCraft/master/tester.lua tester
+
+me = peripheral.getMethods("back")
+if me then
+  print(peripheral.getType("back"))
+  for k,v in pairs (me) do
+    print (k .. " | " .. v)
+  end
+else 
+  print("No methods")
+end
