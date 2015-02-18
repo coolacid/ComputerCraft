618cd54b52198151a58e4467dfffe0136e995348
CoolAcid <jakendall@gmail.com>
2015-02-18 10:49:05 -0500

    Add Single SubDisplay Lua

---
+++ TwitchSub/subdisplay.lua
@@ -0,0 +1,50 @@
+-- Written By CoolAcid
+-- https://github.com/coolacid/ComputerCraft
+
+-- label set SomeKindOfNameHere
+-- pastebin get WdiT6sR5 bootstrap
+-- bootstrap
+-- github get coolacid/ComputerCraft/master/subdisplay.lua startup
+-- startup
+
+-- Twitch Name of the Streamer
+proxyurl = "http://http://localhost:1337/waffle"
+
+-- SleepTime is how often to grab new data. Set here to one minute.
+-- Set it too fast and twitch will flag you for spam
+-- and stop giving you data
+SleepTime = 60
+
+-- Check to see if the JSON api exists. Otherwise, download it. 
+if not fs.exists('json') then
+	print("JSON API not found - Downloading")
+	shell.run("pastebin get 4nRg9CHU json")
+end
+
+os.loadAPI("json")
+local m = peripheral.find("monitor")
+
+m.setTextColor(colors.blue)
+m.setTextScale(1)
+m.setBackgroundColor(colors.white)
+m.clear()
+
+function getSubs()
+  str = http.get(proxyurl).readAll()
+  obj = json.decode(str)
+  lastsub = json.encodePretty(obj.subscriptions.user.display_name)
+  return lastsub
+end
+
+while true do
+  local status, lastsub = pcall(getSubs)
+
+  m.setCursorPos(1,1)
+  if status then 
+    m.write(lastsub)
+  else
+    m.write("Last Sub: ERROR")
+  end
+
+  sleep(SleepTime)
+end
