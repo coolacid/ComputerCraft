39dda6b3da0453a112d35e61bc7f813cf232bb5f
CoolAcid <jakendall@gmail.com>
2015-03-05 22:09:13 -0500

    clean up some code, add real date usage

--- mcdayssince.lua
+++ mcdayssince.lua
@@ -15,6 +15,8 @@ ResetSide = "left"
 -- What happened - for the text
 What = "Days since last button press"
 
+-- Use real time
+RealTime = true
 
 ----- No need to change anything bellow this point
 
@@ -30,14 +32,28 @@ os.loadAPI("functions")
 
 local m = peripheral.find("monitor")
 
+function getTime(external) 
+    local time
+    if not external then
+      time = os.day()
+    else
+      str = http.get("http://time.gov/widget/dhtml/actualtime.cgi?disablecache=".. os.day()).readAll()
+      local ltime = str:gmatch('"(%w+)"')
+      time = math.floor (ltime() / (1000*1000*60*60*24))
+    end
+    return time
+end
+
 function readfile(reset)
   -- Read file to find the time
+  local time
   if fs.exists(".time") and not reset then
     local h = fs.open(".time", "r")
     time = h.readLine()
+    h.close()
   else
     -- Time state not saved yet, set it to in game time and save it
-    time = os.day()
+    time = getTime(RealTime)
     local h = fs.open(".time", "w")
     h.write(time)
     h.close()
@@ -45,23 +61,31 @@ function readfile(reset)
   return time
 end
 
-time = readfile(false)
+starttime = readfile(false)
 
 while true do
-  diff = os.day() - time
-  print(diff)
+  cTime = getTime(RealTime)
+  diff = cTime - starttime
+  print ("CTime: " .. cTime)
+  print ("StartTime: " .. starttime)
+  print ("Diff: " .. diff)
   text = What .. ": " .. diff
   functions.centerText(m, text)
   m.write(text)
   i = 0
-  while i < 5*2 do
+  if RealTime then
+    waitdelay = 30
+  else 
+    waitdelay = 5
+  end
+  while i < waitdelay * 2 do
     if redstone.getInput(ResetSide) then
-      readfile(true)
+      starttime = readfile(true)
       sleep (1)
+      m.clear()
       break
     end
     sleep (.5)
     i = i + 1
   end
 end
-
